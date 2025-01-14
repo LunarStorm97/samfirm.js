6220087e88882148f852600f21c842487eae4790
LunarStorm97 <ian8388382@proton.me>
2025-01-14 14:31:15 -0300

    Update and rename FUSMsg.ts to FUSMsg.mjs

---
+++ types/FUSMsg.mjs
@@ -0,0 +1,53 @@
+export const FUSMsg = {
+  FUSMsg: {
+    FUSHdr: {
+      ProtoVer: '',
+    },
+    FUSBody: {
+      Put: {
+        DEVICE_AID_CODE: {
+          Data: '',
+        },
+        DEVICE_CC_CODE: {
+          Data: '',
+        },
+        MCC_NUM: {
+          Data: '',
+        },
+        MNC_NUM: {
+          Data: '',
+        },
+        ACCESS_MODE: {
+          Data: 0,
+        },
+        BINARY_FILE_NAME: {
+          Data: '',
+        },
+        BINARY_NATURE: {
+          Data: 0,
+        },
+        CLIENT_PRODUCT: {
+          Data: '',
+        },
+        CLIENT_VERSION: {
+          Data: '',
+        },
+        DEVICE_FW_VERSION: {
+          Data: '',
+        },
+        DEVICE_LOCAL_CODE: {
+          Data: '',
+        },
+        DEVICE_MODEL_NAME: {
+          Data: '',
+        },
+        DEVICE_IMEI_PUSH: {
+          Data: '',
+        },
+        LOGIC_CHECK: {
+          Data: '',
+        },
+      },
+    },
+  },
+};
