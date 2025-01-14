c5d5294fb5f2d7ff9b819b55ace1365c895cc67d
LunarStorm97 <ian8388382@proton.me>
2025-01-14 18:45:59 -0300

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -3,7 +3,6 @@
 import crypto from 'crypto';
 import fs from 'fs';
 import path from 'path';
-
 import axios from 'axios';
 import cliProgress from 'cli-progress';
 import { Command } from 'commander';
