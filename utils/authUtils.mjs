303c429691fb59ad6d9823021409f99901e5bb0b
LunarStorm97 <ian8388382@proton.me>
2025-01-14 15:04:42 -0300

    Update authUtils.mjs

--- utils/authUtils.mjs
+++ utils/authUtils.mjs
@@ -1,23 +1,23 @@
-import crypto from "crypto";
+import crypto from 'crypto';
 
-const NONCE_KEY = "vicopx7dqu06emacgpnpy8j8zwhduwlh";
-const AUTH_KEY = "9u7qab84rpc16gvk";
+const NONCE_KEY = 'vicopx7dqu06emacgpnpy8j8zwhduwlh';
+const AUTH_KEY = '9u7qab84rpc16gvk';
 
 export const decryptNonce = (nonceEncrypted) => {
   const nonceDecipher = crypto.createDecipheriv(
-    "aes-256-cbc",
+    'aes-256-cbc',
     NONCE_KEY,
-    NONCE_KEY.slice(0, 16),
+    NONCE_KEY.slice(0, 16)
   );
 
   return Buffer.concat([
-    nonceDecipher.update(nonceEncrypted, "base64"),
+    nonceDecipher.update(nonceEncrypted, 'base64'),
     nonceDecipher.final(),
-  ]).toString("utf-8");
+  ]).toString('utf-8');
 };
 
 export const getAuthorization = (nonceDecrypted) => {
-  let key = "";
+  let key = '';
   for (let i = 0; i < 16; i += 1) {
     const nonceChar = nonceDecrypted.charCodeAt(i);
     key += NONCE_KEY[nonceChar % 16];
@@ -25,15 +25,15 @@ export const getAuthorization = (nonceDecrypted) => {
   key += AUTH_KEY;
 
   const authCipher = crypto.createCipheriv(
-    "aes-256-cbc",
+    'aes-256-cbc',
     key,
-    key.slice(0, 16),
+    key.slice(0, 16)
   );
 
   return Buffer.concat([
-    authCipher.update(nonceDecrypted, "utf8"),
+    authCipher.update(nonceDecrypted, 'utf8'),
     authCipher.final(),
-  ]).toString("base64");
+  ]).toString('base64');
 };
 
 export const handleAuthRotation = (responseHeaders) => {
