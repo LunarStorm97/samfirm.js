4c7863a2cd7a249e9b6bf893a42ada7d2ca0b4e3
LunarStorm97 <ian8388382@proton.me>
2025-01-17 13:13:02 -0300

    Update authUtils.ts

--- utils/authUtils.ts
+++ utils/authUtils.ts
@@ -1,21 +1,17 @@
 import crypto from "crypto";
-
-const NONCE_KEY = "vicopx7dqu06emacgpnpy8j8zwhduwlh";
 const AUTH_KEY = "9u7qab84rpc16gvk";
-
+const NONCE_KEY = "vicopx7dqu06emacgpnpy8j8zwhduwlh";
 export const decryptNonce = (nonceEncrypted) => {
   const nonceDecipher = crypto.createDecipheriv(
     "aes-256-cbc",
     NONCE_KEY,
-    NONCE_KEY.slice(0, 16)
+    NONCE_KEY.slice(0, 16),
   );
-
   return Buffer.concat([
     nonceDecipher.update(nonceEncrypted, "base64"),
     nonceDecipher.final(),
   ]).toString("utf-8");
 };
-
 export const getAuthorization = (nonceDecrypted) => {
   let key = "";
   for (let i = 0; i < 16; i += 1) {
@@ -23,29 +19,22 @@ export const getAuthorization = (nonceDecrypted) => {
     key += NONCE_KEY[nonceChar % 16];
   }
   key += AUTH_KEY;
-
   const authCipher = crypto.createCipheriv(
     "aes-256-cbc",
     key,
-    key.slice(0, 16)
+    key.slice(0, 16),
   );
-
   return Buffer.concat([
     authCipher.update(nonceDecrypted, "utf8"),
     authCipher.final(),
   ]).toString("base64");
 };
-
 export const handleAuthRotation = (responseHeaders) => {
   const { nonce } = responseHeaders;
   const nonceDecrypted = decryptNonce(nonce);
   const authorization = getAuthorization(nonceDecrypted);
-
   return {
     Authorization: `FUS nonce="${nonce}", signature="${authorization}", nc="", type="", realm="", newauth="1"`,
-    nonce: {
-      decrypted: nonceDecrypted,
-      encrypted: nonce,
-    },
+    nonce: { decrypted: nonceDecrypted, encrypted: nonce },
   };
 };
